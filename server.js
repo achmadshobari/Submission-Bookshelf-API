291791b6b7fe90e227a5b6e9a75858c1572895c1
achmadshobari <achmadshobari@gmail.com>
2024-05-21 22:21:02 +0700

    memperbaiki routing

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const http = require('http');
 
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-
-    response.statusCode = 200;
-
+    response.setHeader('Content-Type', 'text/html'); 
+    
     const {url,method} = request;
 
     if(url==='/'){
         if (method === "GET"){
+            response.statusCode = 200;
             response.end(`<h1>Request ini menggunakan method: ${method} untuk mengakses halaman homepage</h1>`);
         }else{
+            response.statusCode = 400;
             response.end(`<h1> Halaman tidak dapat diakses dengan ${method} request</h1>`)
         }        
     }
@@ -20,7 +20,8 @@ const requestListener = (request, response) => {
     if(url==='/about'){
 
         if (method === "GET"){
-            response.end(`<h1>Request ini menggunakan method ${method} untuk mengakses halaman about/h1>`);
+            response.statusCode = 200;
+            response.end(`<h1>Request ini menggunakan method ${method} untuk mengakses halaman about</h1>`);
         
         } else if (method === "POST"){
             let body=[];
@@ -32,14 +33,21 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const {name} = JSON.parse(body);
-                response.end(`<h1> Hai, ${name}!, selamat datang di halaman about</h1`)
+                response.statusCode = 200;
+                response.end(`<h1> Hai, ${name}!, selamat datang di halaman about</h1>`)
     
             });
     
         }else{
+            response.statusCode = 400;
             response.end(`<h1> Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
 
+    } else{
+        response.statusCode = 400;
+        response.end(JSON.stringify({
+            message : 'Halaman tidak ditemukan',
+        }));
     }
     
 
